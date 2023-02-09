950af66e69f3c4ab5d89a747f27a71c199bdec1a
Almas Utebayev <almas7887@gmail.com>
2023-02-09 22:44:40 +0500

    linter added

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
-function greetings() {
-    const name = readlineSync.question('May I have your name?: ');
-    console.log('Hello, ' + name + '!')
-}
 
-export {greetings};
+export default () => {
+  const name = readlineSync.question('May I have your name?: ');
+  console.log('Hello, ' + name + '!');
+};
